fa5ad0af9f3668a9568b3557902cbc85e4baaaa9
小泽 <1416065820@qq.com>
2023-02-11 17:24:27 +0800

    layout&router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,24 +1,37 @@
 import { lazy, Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
 
+import Loading from '../components/loading'
+import NotFount from '../view/notFount'
+import Layout from '../layout/Layout'
+
 const Home = lazy(() => import('../view/home'))
 const Login = lazy(() => import('../view/login'))
 
-
 const route = [
-  {
-    path: '/home',
-    element: <Home/>
-  },
   {
     path: '/login',
     element: <Login/>
+  },
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: 'home',
+        element: <Home/>
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFount/>
   }
 ]
 
 function Router() {
   return(
-    <Suspense fallback={<div>loading</div>}>
+    <Suspense fallback={<Loading></Loading>}>
       <RouteList/>
     </Suspense>
   )
