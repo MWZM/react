e576c46cd4e0136058723dda19ed85c9e5f901e6
小泽 <1416065820@qq.com>
2023-02-14 18:28:14 +0800

    注释添加

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,3 +1,6 @@
+/**
+ * @Outlet 路由占位符号 类似 Vue router-view 组件
+ */
 import { Outlet } from "react-router-dom"
 
 function Layout() {
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,3 +1,10 @@
+/**
+ * @lazy 路由懒加载
+ * @Suspense 路由看加载进入页面会有延迟，使用组件可优化交互，fallback 属性接受任何在组件加载过程中你想展示的 React 元素
+ * @notFound 放在最后用于兜底，当路由不匹配时进入该页面
+ * @useRoutes 具体查看 readme
+ */
+
 import { lazy, Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
 
